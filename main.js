9f2b50870dd581c77a4a2cedf124e81e17546b6f
Harry Yuan <harryuan.65@gmail.com>
2020-08-21 18:03:48 +0800

    feat: add init loading effect/ format css indentation

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 screenTitle = "harry@Harrysde-MacBook-Pro: ~";
+const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+const dowNames =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 document.addEventListener("DOMContentLoaded", ()=>{
+    let date = new Date();
+    document.getElementById('records').innerHTML=`Last login: ${dowNames[date.getDay()].slice(0,3)} ${monthNames[date.getMonth()].slice(0,3)} ${date.getDate()} ${date.toLocaleTimeString()} on ttys002<br>`;
+    setTimeout(()=>Init(), Math.random()*1500 + 400);
+})
+
+function Init(){
     document.getElementById('title').innerText = screenTitle;
     document.getElementById('typezone-prefix').innerText = screenTitle;
     document.getElementById('typezone').addEventListener('keypress', (e)=>{
         if(e.key === 'Enter' || e.keyCode === 13){
-          let newLine = screenTitle + " " + e.target.value + "<br>";
-          document.getElementById("records").innerHTML+=newLine;
-          console.log(e.target);
-          console.log(newLine);
-          e.target.value = "";
+          addHistory(e.target.value);
         }
     })
-})
-function add_history(text){
-  var records = document.getElementById('records');
-  var prev_content = records.innerHTML;
+}
+function addHistory(text){
+    let newLine = screenTitle + " " + text + "<br>";
+    document.getElementById("records").innerHTML+=newLine;
+    document.getElementById('typezone').value="";
 }
