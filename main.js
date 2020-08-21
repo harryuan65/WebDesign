de3c5cceeada59a9f5f12c697e38d315e4d707f4
Harry Yuan <harryuan.65@gmail.com>
2020-08-21 17:45:34 +0800

    feat: load title/ typezone prefix/ add new line js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+screenTitle = "harry@Harrysde-MacBook-Pro: ~";
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.getElementById('title').innerText = screenTitle;
+    document.getElementById('typezone-prefix').innerText = screenTitle;
+    document.getElementById('typezone').addEventListener('keypress', (e)=>{
+        if(e.key === 'Enter' || e.keyCode === 13){
+          let newLine = screenTitle + " " + e.target.value + "<br>";
+          document.getElementById("records").innerHTML+=newLine;
+          console.log(e.target);
+          console.log(newLine);
+          e.target.value = "";
+        }
+    })
+})
 function add_history(text){
   var records = document.getElementById('records');
   var prev_content = records.innerHTML;
