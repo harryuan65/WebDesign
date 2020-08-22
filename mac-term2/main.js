1de44bbae1aee4ad81eaa4a109e8e6c41568de11
Harry Yuan <harryuan.65@gmail.com>
2020-08-22 15:01:07 +0800

    feat: add ls and -al

--- mac-term2/main.js
+++ mac-term2/main.js
@@ -1,3 +1,4 @@
+LOAD_TIME = 300; //ms
 screenTitle = "harry@Harrysde-MacBook-Pro: ~";
 const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"];
@@ -45,30 +46,104 @@ function dragElement(elmnt) {
   }
 }
 //end drag functions
-
 document.addEventListener("DOMContentLoaded", ()=>{
     let w = document.getElementById("window");
     if(w){
       dragElement(w);
       let date = new Date();
       document.getElementById('records').innerHTML=`Last login: ${dowNames[date.getDay()].slice(0,3)} ${monthNames[date.getMonth()].slice(0,3)} ${date.getDate()} ${date.toLocaleTimeString()} on ttys002<br>`;
-      setTimeout(()=>Init(), Math.random()*1000 + 500);
+      setTimeout(()=>Init(), Math.random()*100 + LOAD_TIME);
     }
+    // document.getElementById('body').addEventListener('click', ()=>{
+    //   document.getElementById("typezone").focus();
+    // })
 })
-
 function Init(){
     if(document.getElementById("window")){
       document.getElementById('title').innerText = screenTitle;
       document.getElementById('typezone-prefix').innerText = screenTitle;
       document.getElementById('typezone').addEventListener('keypress', (e)=>{
           if(e.key === 'Enter' || e.keyCode === 13){
-            addHistory(e.target.value);
+            handleCmd(e.target.value);
           }
       })
+      document.getElementById("typezone").focus();
     }
 }
-function addHistory(text){
-    let newLine = screenTitle + " " + text + "<br>";
+function addHistory(text, response=""){
+    // let newLine = screenTitle + " " + text + "<br>";
+    let repeatInput = `${screenTitle} ${text}<br>`;
+    let newLine = repeatInput + response + '<br>';
     document.getElementById("records").innerHTML+=newLine;
     document.getElementById('typezone').value="";
 }
+function handleCmd(text){
+  var regexes = [/^ls\s*$|^ls(\s-[a|l|s|h]*)*$/g, ];
+  var cmdMatchGroup = [];
+  text = text.trim(); //remove trailing white space;
+  regexes.forEach(reg=>{
+    cmdMatchGroup = text.match(reg);
+    if(cmdMatchGroup){
+      return
+    }
+  })
+  if(cmdMatchGroup){
+    var cmds = cmdMatchGroup[0].includes(' ') ? cmdMatchGroup[0].split(' ') : [cmdMatchGroup[0]];
+    switch(cmds[0]){
+      case "ls":
+        let opts = cmds[1] ? cmds[1].split('') : [];
+        if(opts.length > 0 && opts[0]!=="-"){
+          addHistory(opts.slice(1,opts.length).join() + ": No such file or directory");
+        }
+        else{
+          addHistory(text, ls(opts));
+        }
+        break;
+      default:
+        addHistory(text, `zsh: command not found/supported: ${text}`);
+    }
+  }
+  else{
+    addHistory(text, `zsh: command not found/supported: ${text}`);
+  }
+}
+
+function ls(opts=[]){
+  // for now supports -a -l -s -h
+  let lines = [];
+  let inline = false;
+  if (opts.includes("l")){
+    lines =  [
+      "drwxr-xr-x 5 harry staff 160 8 22 12:37 .",
+      "drwxr-xr-x 6 harry staff 192 8 22 12:37 ..",
+      "-rw-r--r-- 1 harry staff 1150 8 21 18:50 index.html",
+      "-rw-r--r-- 1 harry staff 2581 8 22 12:37 main.js",
+      "-rw-r--r-- 1 harry staff 3007 8 21 20:51 style.css",
+    ];
+    let lengthMapping = [10, 1, 5, 5, 6, 1, 2, 4, 0]
+    lines = lines.map(
+      e=>{
+        return e.split(' ').map((part,i)=>{return part.padStart(lengthMapping[i], '^').padEnd(2, '^');}).join('&nbsp;').replace(/\^/g,"&nbsp;")
+      }
+    );
+    console.log(lines);
+    inline = false;
+  }
+  else{
+    lines = [
+      ".", "..", "index.html", "main.js", "style.css"
+    ];
+    inline = true
+  }
+  if (!opts.includes("a")){
+    lines = lines.slice(2, lines.length);
+  }
+  return inline ? lines.join("&nbsp;&nbsp;") : lines.join("<br>") ;
+}
+
+
+
+
+
+
+
